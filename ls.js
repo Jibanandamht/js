5135eef3ddc07d94042b1844f891041c798b4b6b
Jibanandamht <jibanandakoyal420@gmail.com>
2022-05-11 11:53:26 +0530

    new js

---
+++ ls.js
@@ -0,0 +1,50 @@
+let x=0;
+while(x<=10){
+    console.log(x)
+    if(x==5){
+        break;
+    }
+    x++;
+}
+
+
+/*for(let y in x){
+    console.log(x[y])}
+
+
+
+
+const x=['ram','don',]
+console.log(x)
+
+
+
+
+
+
+let x=19;
+let y=16;
+if(x>=y){
+    alert('YOU CAN ENTER TO THIS DOMAIN');
+}else{
+    alert('MAA CHUDAO');
+}
+
+
+//console.log(Math.random())
+
+const names = ['Ram','Shyam','Madhu','Gupta']
+names.push('bawasir')
+document.getElementById("id1").innerHTML=names;
+console.log(names)
+let x=("")
+names.sort()
+names.forEach(loop)
+document.getElementById("id1").innerHTML=x;
+function loop (value,index,array){
+x+=value+"<br>"
+}*/
+
+
+//names.sort()
+//document.getElementById("id1").innerHTML=names;
